e08f881a317e3a6ad78bd6ef9041aad65f708dcc
ground-gun <3280261540@qq.com>
2025-10-10 23:06:46 +0800

    日出日落开放平台

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -109,7 +109,8 @@ module.exports = {
                     {text: 'Gitee', link: 'https://gitee.com/ground-gun'},
                     {text: 'Github', link: 'https://github.com/triggergun'},
                     {text: '知乎', link: 'https://www.zhihu.com/people/hong-lang-80-78/posts'},
-                    {text: '英语', link: 'https://eglish.wutunan.top'}
+                    {text: '英语', link: 'https://eglish.wutunan.top'},
+                    {text: '日出日落开放平台', link: 'https://www.tc03vd.top/sunrise-sunset/zh/'},
                 ]
             },
 
