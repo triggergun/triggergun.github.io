7a3c2f3cebeb98f29cecab22c1baedb9a4eba361
ground-gun <3280261540@qq.com>
2025-10-20 20:59:04 +0800

    美工

--- docs/.vuepress/contents/sidebar/mei_gongPr.js
+++ docs/.vuepress/contents/sidebar/mei_gongPr.js
@@ -6,6 +6,15 @@ module.exports = [
         children: [
             encodeURI('/美工/pr/practical/demo01_2025_10_10_01'),
             encodeURI('/美工/pr/practical/demo01_2025_10_10_02'),
+            encodeURI('/美工/pr/practical/demo01_2025_10_10_03'),
+        ]
+    },
+    {
+        title: 'Pr基础知识补充',
+        collapsable: false,
+        children: [
+            encodeURI('/美工/pr/base/demo01_2025_10_20_01'),
+
         ]
     }
 ];
