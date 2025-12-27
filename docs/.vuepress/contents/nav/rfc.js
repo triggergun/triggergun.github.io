5c01cb1ab45a94aad304e80343888fe0a5557302
ground-gun <3280261540@qq.com>
2025-12-27 17:50:05 +0800

    rfc

---
+++ docs/.vuepress/contents/nav/rfc.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {text: 'rfc概述', link: '/rfc_doc/demo01_2025_12_12_01'},
+    {
+        text: 'http/1.1',
+        items: [
+            {text: "http/1.1", link: ''},
+        ]
+    },
+];
---
+++ docs/.vuepress/contents/sidebar/rfcDoc.js
@@ -0,0 +1,10 @@
+module.exports = [
+
+    {
+        title: 'rfc概述',
+        collapsable: false,
+        children: [
+            encodeURI('/rfc_doc/demo01_2025_12_12_01'),
+        ]
+    }
+];
