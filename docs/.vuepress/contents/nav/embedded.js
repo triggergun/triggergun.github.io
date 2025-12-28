a427f81670075b4efd5402e4dc0c7b68a1c71111
ground-gun <3280261540@qq.com>
2025-12-28 20:26:52 +0800

    嵌入式

--- docs/.vuepress/contents/nav/embedded.js
+++ docs/.vuepress/contents/nav/embedded.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         text: '乐鑫开发板',
         items: [
-            {text: "esp8266", link: ''},
+            {text: "esp8266", link: '/embedded/esp8266/demo01_2025_12_12_01'},
         ]
     },
 ];
