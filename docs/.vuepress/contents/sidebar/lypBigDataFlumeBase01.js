ab12a85c2a3a9fa9921ba8d4080dd4a4eed8c20b
ground-gun <3280261540@qq.com>
2026-03-11 12:51:49 +0800

    bigdata

--- docs/.vuepress/contents/sidebar/lypBigDataFlumeBase01.js
+++ docs/.vuepress/contents/sidebar/lypBigDataFlumeBase01.js
@@ -4,7 +4,7 @@ module.exports = [
     {
         title: 'flume基础一阶段',
         collapsable: false,
-        children: [encodeURI('/lyp/大数据/flume/base01/版本管理'),
+        children: [encodeURI('/lyp/bigdata/flume/base01/版本管理'),
         ]
     },
 ];
--- docs/.vuepress/contents/sidebar/lypBigDataScalaBase.js
+++ docs/.vuepress/contents/sidebar/lypBigDataScalaBase.js
@@ -3,7 +3,7 @@ module.exports = [
         title: 'scala语言基础',
         collapsable: false,
         children: [
-            encodeURI('/lyp/大数据/scala语言基础/01scala简介'),
+            encodeURI('/lyp/bigdata/scala语言基础/01scala简介'),
         ]
 
     },
